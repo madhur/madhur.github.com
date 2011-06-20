fd18ecf3627c40f9c588a5023b436647d4237b87
madhur <madhur@madhur-PC.(none)>
2011-06-19 21:48:42 -0400

    added files

---
+++ _plugins/projects.rb
@@ -0,0 +1,53 @@
+require 'custom_page.rb'
+
+module Jekyll
+  class Project < CustomPage
+    def initialize(site, base, dir, name, info)
+      super site, base, dir, 'project'
+      puts "Building project: #{name}"
+      
+      self.data['title'] = info['title'] || name
+      self.data['version'] = info['version_title'] || info['version']
+      self.data['repo'] = "https://github.com/#{site.config['github_user']}/#{name}"
+      self.data['download'] = "#{self.data['repo']}/zipball/#{info['version'] || 'master'}"
+      self.data['docs'] = info['docs'] == 'wiki' ? "#{self.data['repo']}/wiki" : info['docs'] if info['docs']
+      self.data['description'] = info['description']
+      
+      # this stuff is bit hackish, but it works
+      readme = `curl https://raw.github.com/#{site.config['github_user']}/#{name}/master/README.md` # this will fail if README.md isn't present
+      readme.gsub!(/\`{3} ?(\w+)\n(.+?)\n\`{3}/m, "{% highlight \\1 %}\n\\2\n{% endhighlight %}")
+      readme.gsub!(/Ö/, '&#214;') # so that I can use LÖVE in my READMEs
+      readme = Liquid::Template.parse(readme).render({}, :filters => [Jekyll::Filters], :registers => { :site => site })
+      self.data['readme'] = RDiscount.new(readme).to_html
+    end
+  end
+  
+  class Projects < CustomPage
+    def initialize(site, base, dir, projects)
+      super site, base, dir, 'projects'
+      data = []
+      projects.each { |v| data << v.data }
+      self.data['projects'] = data
+    end
+  end
+  
+  class Site
+    def generate_projects
+      return unless self.config.key? 'projects'
+      throw "No 'project' layout found." unless self.layouts.key? 'project'
+      dir = self.config['project_dir'] || 'projects'
+      projects = []
+      
+      self.config['projects'].each do |k, v|
+        slug = v['slug'] || k.slugize
+        p = Project.new(self, self.source, File.join(dir, slug), k, v)
+        p.data['url'] = "/#{dir}/#{slug}"
+        projects << p
+        write_page p
+      end
+      
+      write_page Projects.new(self, self.source, dir, projects) if self.layouts.key? 'projects'
+    end
+  end
+end
+
--- blogindex.markdown
+++ blogindex.markdown
@@ -7,35 +7,33 @@ keywords: C/C++, Windbg, Reverse Engineering, important
 important: yes
 ---
 
-Blog
+<style>
+div.post
+{
+width: 55em;
+}
+</style>
+
+Blog [![Feed icon](/files/css/feed-icon-14x14.png){:title="Atom feed of recent posts" .right}][feed]
 =====================
 
-More [information](/info) about this blog and a complete archive of [past](past.html) posts, are 
-available via links at the top of the page.
+{% for post in site.posts limit: listing_limit %}
+  <h2><a href="{{ post.url }}">{{ post.title }}</a></h2>
+  <h4 style="float:left">
+    Posted on {{ post.date | format_date | date_to_long_string }}
+ 
+  </h4>
+ <span style="float:right"><a class="comments" data-disqus-identifier="{{ post.disqus_id }}" href="{{ post.url }}#disqus_thread">View Comments</a></span>  
 
-[![Feed icon](/files/css/feed-icon-14x14.png){:title="Atom feed of recent posts" .right}][feed]
-A [feed][] of the most recent posts is also available.
+<div class="c">&nbsp;</div>
+  <p>{{ post.content | strip_html | truncatewords: 75 }}</p>
+  <p><a href="{{ post.url }}">Read more...</a></p>
+{% endfor %}
+
 
 [feed]: {% include feedname.txt %}
 
-###Latest Blog updates###
 
-<table id="highlight" cellpadding="0" cellspacing="0" border="0">
-{% for post in site.posts %}
-
-
-<tr>
-
-  
-  <td class="title"><a  href="{{ post.url }}" data-disqus-identifier="{{ post.disqus_id }}">{{ post.title }}</a></td>
-  <td class="date">{{ post.date | date_to_string }}</td>
-  <td class="time"><a class="comments" data-disqus-identifier="{{ post.disqus_id }}" href="{{ post.url }}#disqus_thread">View Comments</a></td>
-  
-
-</tr>
-
-{% endfor %}
-</table>
 
 <p>
 <a href="past.html">Older Posts &rarr;</a>
