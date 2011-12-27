996f72d673a3285eff63d422dc190579e79c4b93
root <root@bt.foo.org>
2011-12-27 23:30:42 +0530

    added script

---
+++ deploy.cmd
@@ -0,0 +1,14 @@
+git checkout source
+do whatever you need
+git status / add / commit
+jekyll
+checkout master
+execute
+
+cp -r _site/* . && rm -rf _site/ && touch .nojekyll
+
+git status
+
+git add / commit
+
+git push -all origin
